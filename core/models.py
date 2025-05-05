c697116abec5a606024fdb1fd3d15623f73dd31c
Laksh <laksh8396@gmail.com>
2025-05-05 18:47:58 +0530

    add seller models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Buyer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Or just store name/email
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)  # or Seller model
+    available_quantity = models.IntegerField()
+
+class Order(models.Model):
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('paid', 'Paid')])
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE) # related name allows 
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # price at the time of order
+
+class Invoice(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to='/invoices')
+
+class ShippingInfo(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    address = models.TextField()
+    shipping_status = models.CharField(max_length=20)
